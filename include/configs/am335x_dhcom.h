8b486b2c749e5e529ea4fc2efc43e3e4d7aa483c
Ludwig Zenz <lzenz@dh-electronics.de>
2015-03-30 10:00:18 +0200

    config: disable usb boot for spl

--- include/configs/am335x_dhcom.h
+++ include/configs/am335x_dhcom.h
@@ -224,7 +224,7 @@
 #define CONFIG_SYS_BOOTCOUNT_BE
 
 /* USB gadget RNDIS */
-#define CONFIG_SPL_MUSB_NEW_SUPPORT
+/* #define CONFIG_SPL_MUSB_NEW_SUPPORT */
 
 #define CONFIG_SPL_LDSCRIPT		"$(CPUDIR)/am33xx/u-boot-spl.lds"
 #endif
